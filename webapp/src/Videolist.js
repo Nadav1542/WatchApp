8481ed30379f8100000293219bf6535775a3b3f7
Nadav1542 <nadavi1542@gmail.com>
2024-06-02 08:45:26 +0300

    changing Videolist component

--- webapp/src/Videolist.js
+++ webapp/src/Videolist.js
@@ -1,14 +1,17 @@
 import movies from './videos.json'
 import VideoItem from './videoItem/VideoItem';
 
-//component of videolist. function gets array of videos objects and creates
-//list of videos in html page.
-function Videolist({videosObj}){
+
+function Videolist({moviesObj}){
+    
+    
     return (
 <>
-    {videosObj.map((video, key) =>{return<VideoItem title={video.title} description={video.description} image={video.image} key={key}/>})}
+    {moviesObj.map((video, key) =>{return<VideoItem title={video.title} description={video.description} image={video.image} key={key}/>})}
 </>
     );
+
+
 }
 
 export default Videolist;
