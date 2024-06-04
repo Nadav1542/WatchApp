9812dd338586c0c7a6f736320615c77423ed3394
nitay246 <nitay246@gmail.com>
2024-06-04 17:16:38 +0300

    play video when hovering

--- webapp/src/videoItem/VideoItem.js
+++ webapp/src/videoItem/VideoItem.js
@@ -1,20 +1,35 @@
+import React, { useRef } from 'react';
 
+function VideoItem({ title, description, source }) {
+  const videoRef = useRef(null);
 
+  const handleMouseEnter = () => {
+    videoRef.current.play();
+  };
 
-function VideoItem({title, description, source}){
-return(
-<div className="card col-md-4 col-lg-3 col-sm-6 border-0 p-2" >
-                        
-                        <video src={source} className="card-img-top" autoPlay muted loop/>
-                        <div className="card-body">
-                            <p className="card-text">{title}</p>
-                            <p className="card-text">{description}</p> 
-                            <p className="card-text">100M views - 1 week ago</p>
-                        </div>
-                        
-                      </div>
-                      
-);
+  const handleMouseLeave = () => {
+    videoRef.current.pause();
+    videoRef.current.currentTime = 0;
+  };
+
+  return (
+    <div className="card col-md-4 col-lg-3 col-sm-6 border-0 p-2">
+      <video
+        src={source}
+        className="card-img-top"
+        ref={videoRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        muted
+        loop
+      />
+      <div className="card-body">
+        <p className="card-text">{title}</p>
+        <p className="card-text">{description}</p>
+        <p className="card-text">100M views - 1 week ago</p>
+      </div>
+    </div>
+  );
 }
 
 export default VideoItem;
