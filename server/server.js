3976ed8bcebc94325e9d17ae80a084589c831f00
Nadav1542 <nadavi1542@gmail.com>
2024-06-27 19:10:40 +0300

    connecting videos t server

--- server/server.js
+++ server/server.js
@@ -3,24 +3,22 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import videoRoutes from './routes/videoRoutes.js';
-//import Video from './models/Video.js'
 import { getAllVideos } from './controllers/videoController.js';
 
 // Create an Express server
 const server = express();
 server.use(bodyParser.json());
+
 // Middleware
 server.use(cors());
 server.use(express.json());
 
 
-console.log(1)
 // Route to get all videos
 server.get('/api/videos', async (req, res) => {
-  console.log(21)
   try {
     const videos = await getAllVideos();
-    console.log(videos)
+    
     res.json(videos);
   } catch (err) {
     res.status(500).send('Server Error');
@@ -29,7 +27,6 @@ server.get('/api/videos', async (req, res) => {
 
 // Routes
 server.use('/api', videoRoutes);
-console.log(3)
 
 
 // Start the server
