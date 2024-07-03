6cfe448681e087c63af622f6786c01682b5af9a5
nitay246 <nitay246@gmail.com>
2024-07-04 00:47:36 +0300

    Code warning handling

--- server/server.js
+++ server/server.js
@@ -14,10 +14,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/data', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/data');
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
