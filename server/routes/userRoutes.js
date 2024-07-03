0c82fc4dac6855a9987a6f223165c241ff522963
nitay246 <nitay246@gmail.com>
2024-07-03 10:33:09 +0300

    update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,24 +1,14 @@
 import express from 'express';
-import { getUserByUsername, createUser } from '../models/users.js';
+import {logIn, signup} from '../controllers/userController.js'
 import multer from 'multer';
-import { signup, signup as signupController } from '../controllers/userController.js';
+
 
 const router = express.Router();
-const upload = multer(); // Define multer for handling multipart/form-data
+const upload = multer();
 
-// Login route
-router.get('/users/:username/:password', async (req, res) => {
-  const { username, password } = req.params;
-  try {
-    const user = await getUserByUsername(username, password);
-    console.log('message from login route', user);
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(401).json({ error: 'Incorrect username or password' });
-  }
-});
 
-// Signup route
-router.post('/users').post(signup);
+router.route('/:username/:password').get(logIn);
+router.route('/', upload.single('img'), signup)
+
 
 export default router;
