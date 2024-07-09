488845273279d7958082466908259900be2d3430
Nadav1542 <nadavi1542@gmail.com>
2024-07-09 20:58:27 +0300

    check 2

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,20 +1,19 @@
 import express from 'express';
-import {getUserInfo, getUserVideos, signup, generateTokenForUser } from '../controllers/userController.js';
+import {  getUserByHandler,signup, generateTokenForUser } from '../controllers/userController.js';
 import {getVideobyUser,deleteVideo} from '../controllers/videoController.js'
-
+import multer from 'multer';
 
 
 const router = express.Router();
+const upload = multer();
 
 
 //router.route('/:username/:password').get(logIn);
 router.route('/:creator/videos/:id').get(getVideobyUser)
 router.delete('/:creator/videos/:id', deleteVideo);
+router.put('/:creator/videos/:id', deleteVideo);
 router.post('/tokens', generateTokenForUser);
 router.route('/').post(signup)
-router.route('/:id').get(getUserInfo)
-router.route('/:id/videos').get(getUserVideos)
-// router.route('/:id/videos').post(addingVideo)
 
 router.get('/:id', getUserByHandler);
 export default router;
