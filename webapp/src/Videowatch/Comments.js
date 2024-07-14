a74fd761c06d07b32e957bee23b0e88bad88f2f2
nitay246 <nitay246@gmail.com>
2024-07-14 15:41:56 +0300

    minor fix in likes

--- webapp/src/Videowatch/Comments.js
+++ webapp/src/Videowatch/Comments.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import '../Topbar/Searchbar.css';
+import './Comments.css';
 import { UserContext } from '../contexts/UserContext';
 
 
